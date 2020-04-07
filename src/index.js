9faf2ea9e9e07bbdd1411322f242a6848b98f3cc
Jan Vaillant <6503378+jvail@users.noreply.github.com>
2020-04-07 13:00:25 +0200

    Export func

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import worker_str from './tmp/import-worker';
-const workerScript = URL.createObjectURL(new Blob([atob(worker_str)], { type: 'text/javascript' }));
-const worker = new Worker(workerScript);
-export default worker;
+const getWorker = () => {
+    const workerScript = URL.createObjectURL(new Blob([atob(worker_str)], { type: 'text/javascript' }));
+    return new Worker(workerScript);
+}
+export default getWorker;
